2344b03fe2edef3f5ba4663068fd173c3b7fc62a
Zhiguang Xu <xuzhiguang@gmail.com>
2013-01-16 23:03:44 -0500

    Just created a new rails project depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c8b62dc6dfd4fc0f31654e985ec62b3e44855ced68b8d63edc605c56803bc08a8c8480e8a7fdb0e5a7c8a85aabef4522daba4a0c605f057e0ebd4874dafb989d'
