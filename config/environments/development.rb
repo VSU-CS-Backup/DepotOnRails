31d1cfbf64dc43c9c9e30de73abc2682c94b67dc
Zhiguang Xu <xuzhiguang@gmail.com>
2013-01-31 10:44:09 -0500

    Finished Task H.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,17 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { 
+    address: "pod51004.outlook.com",
+    port: 587, 
+    authentication: "login",
+    user_name: "zxu@valdosta.edu", 
+    password: "valdosta2010", 
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
