9dc487433cc8e70da10b246f28114971c6029f23
Zhiguang Xu <xuzhiguang@gmail.com>
2013-03-14 10:57:20 -0400

    Updated sessions_controller.rb such that it is not able to handle HTTP requests that carry ..login.json and ...logout.json

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,14 +15,26 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      respond_to do |format|
+        format.html {redirect_to admin_url}
+        format.xml  { render :text => "Valid user/password combination" }
+      end
+      #redirect_to admin_url
     else
-      redirect_to login_url, alert: "Invalid user/password combination"
+      respond_to do |format|
+        format.html {redirect_to login_url, :alert => "Invalid user/password combination" }
+        format.xml  { render :text => "Invalid user/password combination" }
+      end
+      #redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+        respond_to do |format|
+        format.html {redirect_to store_url, :notice => "Logged out" }
+        format.xml  { render :text => "Logged out" }
+      end
+    #redirect_to store_url, notice: "Logged out"
   end
 end
